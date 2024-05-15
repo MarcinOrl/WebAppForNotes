367ff4f65dc69fc71709f8adedfeddd24c20c06e
Martini <marcino064@o2.pl>
2024-05-15 13:35:29 +0200

    Dodano integrację Tailwind CSS i przykładowy widok

--- notes_project/tailwind.config.js
+++ notes_project/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './notes_project/**/*.html',
+    './notes_app/**/*.html',
+  ],
   theme: {
     extend: {},
   },
